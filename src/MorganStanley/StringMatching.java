1767b1a33cf4cc8161f148d406041e6c8a3bbdda
Wang, Chen <zhanghuilian-98022@163.com>
2019-03-28 17:51:45 +0800

    Added the earliestCharacter algorithm

--- src/MorganStanley/StringMatching.java
+++ src/MorganStanley/StringMatching.java
@@ -8,7 +8,19 @@ public class StringMatching {
      * @return The earliest appearing character with only once appearance.
      */
     private char earliestCharacter(String string) {
-
+        int[] count = new int[26];
+        int[] index = new int[26];
+        for (int i = 0; i < string.length(); i++) {
+            count[string.charAt(i) - 'a']++;
+            int offset = string.charAt(i) - 'a';
+            if (index[offset] == 0) index[offset] = i;
+        }
+        int pos = string.length();
+        for (int i = 0; i < 26; i++) {
+            if (count[i] == 1)
+                if (index[i] != -1 && index[i] < pos) pos = index[i];
+        }
+        if (pos < string.length()) return string.charAt(pos);
         return 0;
     }
 }
