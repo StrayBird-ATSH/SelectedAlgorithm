a83e5f1c44fd07e86bc9c0bd8e60682c2c2ff9f4
Wang, Chen <zhanghuilian-98022@163.com>
2019-03-28 16:45:59 +0800

    Some morgan stanley AC problems

---
+++ src/MorganStanley/StringMatching.java
@@ -0,0 +1,14 @@
+package MorganStanley;
+
+public class StringMatching {
+    /**
+     * Finds the first character that only appears once in the String.
+     *
+     * @param string The query string
+     * @return The earliest appearing character with only once appearance.
+     */
+    private char earliestCharacter(String string) {
+
+        return 0;
+    }
+}
